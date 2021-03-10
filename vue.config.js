392a13287f960621709af57a624b54632959b95f
13025168803 <48861934+13025168803@users.noreply.github.com>
2021-03-11 01:22:39 +0800

    elementui组件按需引入

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+
+
+// 导包
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 匹配文件名
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+// 配置
+module.exports = {
+	publicPath: "./",
+	
+	assetsDir: "static",
+	
+	outputDir: 'dist',
+	
+ configureWebpack: config => {
+   if (process.env.NODE_ENV === 'production') {
+     return {
+       plugins: [
+         new CompressionWebpackPlugin({
+           filename: '[path].gz[query]',
+           algorithm: 'gzip',  // 默认为gzip
+           test: productionGzipExtensions,
+           threshold: 2048, // 对超过2k的数据进行压缩 
+           minRatio: 0.8,  // 仅压缩比该比率更好的（minRatio = Compressed Size / Original Size）
+           deleteOriginalAssets: false  // 是否删除原文件
+         })
+       ]
+     }
+   }
+ }
+}
